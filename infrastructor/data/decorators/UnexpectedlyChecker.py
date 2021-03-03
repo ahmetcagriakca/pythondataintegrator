ae9147635ef42c919f839f9116c6f748ed59537a
ahmetcagriakca <ahmetcagriakca@gmail.com>
2021-03-04 02:33:25 +0300

    session commit and rollback scenario fixed

--- infrastructor/data/decorators/UnexpectedlyChecker.py
+++ infrastructor/data/decorators/UnexpectedlyChecker.py
@@ -2,15 +2,14 @@
 def transaction_handler(func):
     def inner(*args, **kwargs):
         try:
-            args[0].database_session_manager.close()
             args[0].database_session_manager.connect()
             result= func(*args, **kwargs)
             args[0].database_session_manager.commit()
-            args[0].database_session_manager.close()
+            # args[0].database_session_manager.close()
             return result
         except Exception as ex:
             args[0].database_session_manager.rollback()
-            args[0].database_session_manager.close()
+            args[0].database_session_manager.connect()
             print(ex)
             raise
 
