65ea286aa9b9d4c3efef34f92a2bf5a116a667fc
ahmetcagriakca <ahmetcagriakca@gmail.com>
2021-04-06 10:06:19 +0300

    log text changed

--- domain/operation/execution/adapters/execution/ExecuteIntegrationAdapter.py
+++ domain/operation/execution/adapters/execution/ExecuteIntegrationAdapter.py
@@ -49,26 +49,26 @@ class ExecuteIntegrationAdapter(ExecuteAdapter, IScoped):
         target_connection = self.data_integration_connection_service.get_target_connection(
             data_integration_id=data_integration_id)
         if target_connection.Database is not None:
-            log = f"{target_connection.Database.Schema}.{target_connection.Database.TableName} integration execute operation started"
+            log = f"{target_connection.Database.Schema}.{target_connection.Database.TableName} integration execute started"
         elif target_connection.File is not None:
-            log = f"{target_connection.File.Folder}\\{target_connection.File.FileName} integration execute operation started"
+            log = f"{target_connection.File.Folder}\\{target_connection.File.FileName} integration execute started"
         elif target_connection.Queue is not None:
-            log = f"{target_connection.Queue.TopicName} integration execute operation started"
+            log = f"{target_connection.Queue.TopicName} integration execute started"
         else:
-            log = f"Integration execute operation started"
+            log = f"Integration execute started"
         return log
 
     def get_finish_log(self, data_integration_id: int, data_count: int):
         target_connection = self.data_integration_connection_service.get_target_connection(
             data_integration_id=data_integration_id)
         if target_connection.Database is not None:
-            log = f"{target_connection.Database.Schema}.{target_connection.Database.TableName} integration execute operation finished. (Source Data Count:{data_count})"
+            log = f"{target_connection.Database.Schema}.{target_connection.Database.TableName} integration execute finished. (Source Data Count:{data_count})"
         elif target_connection.File is not None:
-            log = f"{target_connection.File.Folder}\\{target_connection.File.FileName} integration execute operation finished. (Source Data Count:{data_count})"
+            log = f"{target_connection.File.Folder}\\{target_connection.File.FileName} integration execute finished. (Source Data Count:{data_count})"
         elif target_connection.Queue is not None:
-            log = f"{target_connection.Queue.TopicName} integration execute operation finished. (Source Data Count:{data_count})"
+            log = f"{target_connection.Queue.TopicName} integration execute finished. (Source Data Count:{data_count})"
         else:
-            log = f"Integration execute operation finished"
+            log = f"Integration execute finished"
 
         return log
 
@@ -76,13 +76,13 @@ class ExecuteIntegrationAdapter(ExecuteAdapter, IScoped):
         target_connection = self.data_integration_connection_service.get_target_connection(
             data_integration_id=data_integration_id)
         if target_connection.Database is not None:
-            log = f"{target_connection.Database.Schema}.{target_connection.Database.TableName} integration execute operation getting error"
+            log = f"{target_connection.Database.Schema}.{target_connection.Database.TableName} integration execute getting error"
         elif target_connection.File is not None:
-            log = f"{target_connection.File.Folder}\\{target_connection.File.FileName} integration execute operation getting error"
+            log = f"{target_connection.File.Folder}\\{target_connection.File.FileName} integration execute getting error"
         elif target_connection.Queue is not None:
-            log = f"{target_connection.Queue.TopicName} integration execute operation getting error"
+            log = f"{target_connection.Queue.TopicName} integration execute getting error"
         else:
-            log = f"Integration execute operation getting error"
+            log = f"Integration execute getting error"
         return log
 
     def check_error_raise(self) -> bool:
@@ -103,11 +103,11 @@ class ExecuteIntegrationAdapter(ExecuteAdapter, IScoped):
         if limit is not None and limit > 0:
             if process_count > 1:
                 self.sql_logger.info(
-                    f"{data_operation_integration_order}-{data_integration_code} - operation will execute parallel. {process_count}-{limit}",
+                    f"{data_operation_integration_order}-{data_integration_code} - integration will execute parallel. {process_count}-{limit}",
                     job_id=data_operation_job_execution_id)
             else:
                 self.sql_logger.info(
-                    f"{data_operation_integration_order}-{data_integration_code} - operation will execute serial. {limit}",
+                    f"{data_operation_integration_order}-{data_integration_code} - integration will execute serial. {limit}",
                     job_id=data_operation_job_execution_id)
 
             affected_row_count = self.execute_integration_process.start_integration_execution(
