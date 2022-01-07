80c9dd23d9230d6bf02b10568ef4632addb26543
Ahmet Çağrı AKCA <ahmetcagriakca@gmail.com>
2022-01-07 04:09:36 +0300

    connection sql moved new end point

--- src/api/pdi/host/connection/ConnectionResource.py
+++ src/api/pdi/host/connection/ConnectionResource.py
@@ -4,29 +4,9 @@ from pdip.cqrs import Dispatcher
 
 from pdi.application.connection.DeleteConnection.DeleteConnectionCommand import DeleteConnectionCommand
 from pdi.application.connection.DeleteConnection.DeleteConnectionRequest import DeleteConnectionRequest
-from pdi.application.connection.GetConnection.GetConnectionQuery import GetConnectionQuery
-from pdi.application.connection.GetConnection.GetConnectionRequest import GetConnectionRequest
-from pdi.application.connection.GetConnection.GetConnectionResponse import GetConnectionResponse
 from pdi.application.connection.GetConnectionList.GetConnectionListQuery import GetConnectionListQuery
 from pdi.application.connection.GetConnectionList.GetConnectionListRequest import GetConnectionListRequest
 from pdi.application.connection.GetConnectionList.GetConnectionListResponse import GetConnectionListResponse
-
-
-class ConnectionByIdResource(ResourceBase):
-    @inject
-    def __init__(self,
-                 dispatcher: Dispatcher,
-                 *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.dispatcher = dispatcher
-
-    def get(self, req: GetConnectionRequest) -> GetConnectionResponse:
-        """
-        Get Data Operation Job By Id
-        """
-        query = GetConnectionQuery(request=req)
-        result = self.dispatcher.dispatch(query)
-        return result
 
 
 class ConnectionResource(ResourceBase):
